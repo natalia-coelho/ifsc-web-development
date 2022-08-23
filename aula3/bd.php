5ad9350090e0612cc484f1a0b1852df5a7d9f8c9
natalia-coelho <nataliacoelho199@gmail.com>
2022-08-23 20:02:55 -0300

    aula 03

--- aula3/bd.php
+++ aula3/bd.php
@@ -1,11 +1,15 @@
 <?
 // Arquivo que vai conter todas as configuracoes do banco de dados e unificar as funcoes
 // criar funcao obterConexao()
+// Importar o banco disponibilizado pelo prof
+// Colocar os links para quando clicar abrir outra página buscando os dados do autor, livro etc
+
 
 function obterConexao(){
     // Abre a conexão com o banco de dados
     // $conexao = mysqli_connect(SERVIDOR, USUARIO, SENHA, BANCODEDADOS);
     $conexao = mysqli_connect('localhost', 'root', 'aluno', 'estante'); //em casa alterar esses dados
+    return $conexao
 }
 
 function pesquisarAutoresPorCodigoLivro($codigo){
@@ -16,6 +20,6 @@ function pesquisarListaAutores(){
     $conexao = obterConexao();
     $comandoSql = "select * from autor";
     $query = mysqli_query($conexao, $comandoSql);
-    $resultado - mysqli_fetch_all($query, MYSQLI_ASSOC);
+    $resultado = mysqli_fetch_all($query, MYSQLI_ASSOC);
     return $resultado;
 }
