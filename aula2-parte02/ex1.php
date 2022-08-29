034ee1fd2ec76b149c6c540819c7202d2e5f1267
natalia-coelho <nataliacoelho199@gmail.com>
2022-08-28 22:18:28 -0300

    parte 02

---
+++ aula2-parte02/ex1.php
@@ -0,0 +1,14 @@
+<?php
+/*1) Crie uma função que receba como parâmetro um vetor com 10
+números. A função deverá imprimir:
+Quantidade de números negativos;
+Quantidade de números positivos;
+Quantidade de números pares
+Quantidade de números ímpares*/
+
+echo 'aaaa';
+imprimeNumero();
+function imprimeNumero($vetor = [1, 10, 5]){
+    echo $vetor[1];
+}
+?>
