22f9b79cce07b29345a0126977bb19191326874f
Natalia Coelho Oliveira <43504384+natalia-coelho@users.noreply.github.com>
2022-08-31 20:00:39 -0300

    Create ex2.php

---
+++ aula2-parte02/ex2.php
@@ -0,0 +1,10 @@
+<?php
+/* 2)Crie uma função que receba como parâmetro dois vetores com 10 
+números cada. A função deverá realizar a multiplicação dos dois 
+vetores em cada uma de suas respectivas posições. Armazene o 
+resultado em um terceiro vetor resultante. Ao final, imprima os 
+valores dos três vetores.
+Ex.: vetor_a[0] * vetor_b[0] */
+
+
+?> 
