45d815095ec719b47a1ce36c76d1fad480afe5b2
Natalia Coelho Oliveira <43504384+natalia-coelho@users.noreply.github.com>
2022-08-31 20:28:00 -0300

    Create ex5-calculadora.php

---
+++ aula2-parte02/ex5-calculadora.php
@@ -0,0 +1,11 @@
+<?php
+/*5)Crie uma calculadora simples em HTML com as operações somar, subtrair, multiplicar 
+e dividir. A calculadora deverá permitir a entrada de dois operandos e a seleção (radio 
+button) do operador. Ao clicar no botão Calcular, a página deverá apresentar o 
+resultado da operação na própria página.
+Obs.: Crie uma função para cada operador em um arquivo chamado funcoes.php.
+Obs2.: Use o framework front-end bootstrap (
+https://getbootstrap.com/docs/4.0/components/forms/)*/
+
+
+?>
