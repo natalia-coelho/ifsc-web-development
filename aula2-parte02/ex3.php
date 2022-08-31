e465375961b4831d6f7cb9419e97871824740f84
Natalia Coelho Oliveira <43504384+natalia-coelho@users.noreply.github.com>
2022-08-31 20:23:54 -0300

    Create ex3.php

---
+++ aula2-parte02/ex3.php
@@ -0,0 +1,14 @@
+<?php
+/*3) Crie uma matriz (array muntidimensional) com os dados abaixo. Em 
+seguida, crie uma função que receba como parâmetro essa matriz 
+criada e imprima os dados da matriz utilizando a estrutura de 
+repetição FOR;
+Nome Telefone
+Jhon 30332211
+Bob 20221212
+Clara 98223311
+Linda 44330293
+James 38343321*/
+
+
+?>
