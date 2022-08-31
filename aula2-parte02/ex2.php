aa2d060ffd24aa07e06cd9509f6be1858c28d598
Natalia Coelho Oliveira <43504384+natalia-coelho@users.noreply.github.com>
2022-08-31 20:22:04 -0300

    Update ex2.php

--- aula2-parte02/ex2.php
+++ aula2-parte02/ex2.php
@@ -6,5 +6,18 @@ resultado em um terceiro vetor resultante. Ao final, imprima os
 valores dos três vetores.
 Ex.: vetor_a[0] * vetor_b[0] */
 
+	multiplicaVetores($a=[0, 1, 2, 3], $b=[0, 1, 2, 3]);
 
+	function multiplicaVetores($vet1=array(), $vet2=array()){
+		$vet3=array();
+		if(sizeof($vet1) == sizeof($vet2)){
+			for($i = 0; $i < sizeof($vet1); $i++){
+				$vet3[$i] = $vet1[$i] * $vet2[$i];
+				
+			echo $vet1[$i] .'X' .$vet2[$i] .'=' .$vet3[$i] ."\n";
+			}
+		} else {
+			echo 'Insira vetores do mesmo tamanho!';
+		}
+	}
 ?> 
