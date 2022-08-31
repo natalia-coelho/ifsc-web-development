20dfbaf032d05093a74ac1b379b445acdca54f3a
Natalia Coelho Oliveira <43504384+natalia-coelho@users.noreply.github.com>
2022-08-31 20:26:02 -0300

    Create ex4.php

---
+++ aula2-parte02/ex4.php
@@ -0,0 +1,19 @@
+<?php
+/*4) Crie cinco funções em PHP que recebem como parâmetro 10 registros de pessoas (em uma matriz utilizando 
+chave associativa), conforme abaixo, para cada funcionalidade solicitada.
+● Listar os dados das pessoas acima de 50 anos.
+● Litas os dados de todas as pessoas da cidade da Gaspar.
+● Listar os dados das pessoas que começam com as letras A, B ou C.
+● Listar os dados das pessoas maiores de 60 anos da cidade de Gaspar.
+Registros da matriz:
+nr, nome, idade, cidade
+1,Bob, 15, Gaspar
+2,Dylan, 20, Blumenau
+3,Chris, 50, Blumenau
+4,Anna, 8, Indaial
+5,James, 65, Gaspar
+6,Mary, 81, Gaspar
+7,John, 36, Indaial*/
+
+
+?>
